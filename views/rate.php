f7fa02f72f58d32055905c5458c2211d70d11710
maha hanif <maha.hanif@um6p.ma>
2023-06-12 03:26:27 +0100

    haha

--- views/rate.php
+++ views/rate.php
@@ -2,8 +2,29 @@
 <html>
 <head>
     <?php
-    require_once("../db/dbhinc.php");
     
+    include_once '../db/dbhinc.php';
+    session_start();
+    $logout="false";
+    $connexion = "true";
+    if(isset($_SESSION["USER_ID"])){
+      $logout ="true";
+      $connexion = "false";
+    }
+    
+    $artisan_id = $_GET['artisan_id'];
+    
+    $result = mysqli_query($conn,"SELECT AVG(rating) as avg, artisan_id FROM reviews GROUP BY artisan_id HAVING artisan_id = '$artisan_id' ");
+    $res =mysqli_fetch_array($result);
+    $result1 = mysqli_query($conn,"SELECT count(*) as cnt,artisan_id FROM reviews GROUP BY artisan_id HAVING artisan_id = '$artisan_id' ");
+    $avg=$res['avg'];
+    
+    $res1 =mysqli_fetch_array($result1);
+    $cnt=$res1['cnt'];
+
+
+    //$GET[artisan_id]
+
     ?>
 
 <!-- Font Awesome Icon Library -->
@@ -16,6 +37,7 @@
     <script src="https://code.jquery.com/jquery-3.6.0.min.js" integrity="sha256-/xUj+3OJU5yExlq6GSYGSHk7tPXikynS7ogEvDej/m4=" crossorigin="anonymous"></script>
 	<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.9/umd/popper.min.js" integrity="sha384-ApNbgh9B+Y1QKtv3Rn7W3mgPxhU9K/ScQsAP7hUibX39j7fakFPskvXusvfa0b4Q" crossorigin="anonymous"></script>
 	<script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js" integrity="sha384-JZR6Spejh4U02d8jOt6vLEHfe/JQGiRRSQQxSfFWpi1MquVdAyjUar5+76PVCmYl" crossorigin="anonymous"></script>
+   
 <style>
 * {
   box-sizing: border-box;
@@ -95,66 +117,118 @@ body {
 <body>
 
 
-
-<p>4.1 average based on 254 reviews.</p>
-<hr style="border:3px solid #f1f1f1">
+<?php
+echo "<p>$avg average based on $cnt reviews.</p>"
+?>
+<style amp-custom>
+  .rating {
+    --star-size: 3;  /* use CSS variables to calculate dependent dimensions later */
+    padding: 0;  /* to prevent flicker when mousing over padding */
+    border: none;  /* to prevent flicker when mousing over border */
+    unicode-bidi: bidi-override; direction: rtl;  /* for CSS-only style change on hover */
+    text-align: left;  /* revert the RTL direction */
+    user-select: none;  /* disable mouse/touch selection */
+    font-size: 3em;  /* fallback - IE doesn't support CSS variables */
+    font-size: calc(var(--star-size) * 1em);  /* because `var(--star-size)em` would be too good to be true */
+    cursor: pointer;
+    /* disable touch feedback on cursor: pointer - http://stackoverflow.com/q/25704650/1269037 */
+    -webkit-tap-highlight-color: rgba(0,0,0,0);
+    -webkit-tap-highlight-color: transparent;
+    margin-bottom: 1em;
+    
+  }
+  .form{
+    margin-left: 50%;
+  }
+  /* the stars */
+  .rating > label {
+    display: inline-block;
+    position: relative;
+    width: 1.1em;  /* magic number to overlap the radio buttons on top of the stars */
+    width: calc(var(--star-size) / 3 * 1.1em);
+  }
+  .rating > *:hover,
+  .rating > *:hover ~ label,
+  .rating:not(:hover) > input:checked ~ label {
+    color: transparent;  /* reveal the contour/white star from the HTML markup */
+    cursor: inherit;  /* avoid a cursor transition from arrow/pointer to text selection */
+  }
+  .rating > *:hover:before,
+  .rating > *:hover ~ label:before,
+  .rating:not(:hover) > input:checked ~ label:before {
+    content: "★";
+    position: absolute;
+    left: 0;
+    color: gold;
+  }
+  .rating > input {
+    position: relative;
+    transform: scale(3);  /* make the radio buttons big; they don't inherit font-size */
+    transform: scale(var(--star-size));
+    /* the magic numbers below correlate with the font-size */
+    top: -0.5em;  /* margin-top doesn't work */
+    top: calc(var(--star-size) / 6 * -1em);
+    margin-left: -2.5em;  /* overlap the radio buttons exactly under the stars */
+    margin-left: calc(var(--star-size) / 6 * -5em);
+    z-index: 2;  /* bring the button above the stars so it captures touches/clicks */
+    opacity: 0;  /* comment to see where the radio buttons are */
+    font-size: initial; /* reset to default */
+  }
+  form.amp-form-submit-error [submit-error] {
+    color: red;
+  }
+  </style>
+<hr style="border:3px solid #f1f1f1; margin-right:65%">
 
 <div class="row">
-  <div class="side">
-    <div>5 star</div>
+<form id="rating" method="post" action="rateTreatment.php" >
+  <fieldset class="rating">
+    <input name="rating" type="radio" id="rating5" value="5" on="change:rating.submit">
+    <label for="rating5" title="5 stars">☆</label>
+
+    <input name="rating" type="radio" id="rating4" value="4" on="change:rating.submit">
+    <label for="rating4" title="4 stars">☆</label>
+
+    <input name="rating" type="radio" id="rating3" value="3" on="change:rating.submit">
+    <label for="rating3" title="3 stars">☆</label>
+
+    <input name="rating" type="radio" id="rating2" value="2" on="change:rating.submit" >
+    <label for="rating2" title="2 stars">☆</label>
+
+    <input name="rating" type="radio" id="rating1" value="1" on="change:rating.submit">
+    <label for="rating1" title="1 stars">☆</label>
+  </fieldset>
+  <div class="form-group">
+    <label for="exampleFormControlTextarea1" name ="rating_text">Any comment ?</label>
+    <textarea class="form-control" id="exampleFormControlTextarea1" rows="3"></textarea>
   </div>
-  <div class="middle">
-    <div class="bar-container">
-      <div class="bar-5"></div>
-    </div>
-  </div>
-  <div class="side right">
-    <div>150</div>
-  </div>
-  <div class="side">
-    <div>4 star</div>
-  </div>
-  <div class="middle">
-    <div class="bar-container">
-      <div class="bar-4"></div>
-    </div>
-  </div>
-  <div class="side right">
-    <div>63</div>
-  </div>
-  <div class="side">
-    <div>3 star</div>
-  </div>
-  <div class="middle">
-    <div class="bar-container">
-      <div class="bar-3"></div>
-    </div>
-  </div>
-  <div class="side right">
-    <div>15</div>
-  </div>
-  <div class="side">
-    <div>2 star</div>
-  </div>
-  <div class="middle">
-    <div class="bar-container">
-      <div class="bar-2"></div>
-    </div>
-  </div>
-  <div class="side right">
-    <div>6</div>
-  </div>
-  <div class="side">
-    <div>1 star</div>
-  </div>
-  <div class="middle">
-    <div class="bar-container">
-      <div class="bar-1"></div>
-    </div>
-  </div>
-  <div class="side right">
-    <div>20</div>
-  </div>
+  <input class="btn btn-primary btni"  style="background-color:black; border:none; margin-left:50px" type="submit" name="submit" value="Submit Review">
+  
+  
+  
+</form>
+<?php
+
+if(isset($_POST['submit'])){
+    if(!empty($_POST['rating'])) {
+        $rating =$_POST['rating'];
+        $user_id=$_SESSION['USER_ID'];
+        $review_text= $_POST['rating_text'];
+        $date_reviewed = $date = date('m/d/Y ', time());
+        echo '<div class="alert alert-success">
+            <strong>Success!</strong> 
+        </div>';
+        $query= "INSERT INTO RVIEWES (user_id, artisan_id, rating, review_text, datereviewed)
+        VALUES ('$user_id', '$artisan_id', '$rating', '$review_text', '$date_reviewed');"
+    } else {
+        echo '<div class="alert alert-danger">
+        <strong>Please Rate</strong> 
+      </div>';
+        
+}
+}
+?>
+
 </div>
 
 </body>
