9f467e35f5a361ddbddf4a90c3b8e5e6b295ff9f
unknown <Student@FDELL-FM9JN83.UM6P.local>
2023-06-12 04:40:29 +0100

    css

--- views/artisan_details.php
+++ views/artisan_details.php
@@ -226,41 +226,13 @@ $artisanId = $_GET['artisan_id'];
                 </nav>
             </header>
 
-  <!-- ======= Mobile nav toggle button ======= -->
-  <i class="bi bi-list mobile-nav-toggle d-xl-none"></i>
-
-  <!-- ======= Header ======= -->
-  <header id="header">
-    <div class="d-flex flex-column">
-
-      <div class="profile">
-        <h1 class="text-light"><a href="../index.html"><?php echo $artisan['username']; ?></a></h1>
-        <div class="social-links mt-3 text-center">
-          <a href="#" class="twitter"><i class="bx bxl-twitter"></i></a>
-          <a href="#" class="facebook"><i class="bx bxl-facebook"></i></a>
-          <a href="#" class="instagram"><i class="bx bxl-instagram"></i></a>
-          <a href="#" class="google-plus"><i class="bx bxl-skype"></i></a>
-          <a href="#" class="linkedin"><i class="bx bxl-linkedin"></i></a>
-        </div>
-      </div>
-
-      <nav id="navbar" class="nav-menu navbar">
-        <ul>
-          <li><a href="#hero" class="nav-link scrollto active"><i class="bx bx-home"></i> <span>Home</span></a></li>
-          <li><a href="#about" class="nav-link scrollto"><i class="bx bx-user"></i> <span>About</span></a></li>
-          <li><a href="#portfolio" class="nav-link scrollto"><i class="bx bx-book-content"></i> <span>Portfolio</span></a></li>
-          <li><a href="#services" class="nav-link scrollto"><i class="bx bx-server"></i> <span>Services</span></a></li>
-          <li><a href="#contact" class="nav-link scrollto"><i class="bx bx-envelope"></i> <span>Contact</span></a></li>
-        </ul>
-      </nav><!-- .nav-menu -->
-    </div>
-  </header><!-- End Header -->
+  
 
   <!-- ======= Hero Section ======= -->
   <section id="hero" class="d-flex flex-column justify-content-center align-items-center">
-    <div class="hero-container" data-aos="fade-in">
-      <h1>I'm <?php echo $artisan['username']; ?></h1>
-      <p><?php echo $artisan['description']; ?> </p>
+    <div class="hero-container" data-aos="fade-in" style="width: 650px;">
+      <h1>Je suis <?php echo $artisan['username']; ?></h1>
+      <p style="font-size: 20px;"><?php echo $artisan['description']; ?> </p>
     </div>
   </section><!-- End Hero -->
 
